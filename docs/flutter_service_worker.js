c91931f2a67f7557fddf8beb2b3b0d2dd47d9f7b
AkbarHusainn <akbarhussain700290@gmail.com>
2026-04-06 20:32:47 +0530

    updates

--- docs/flutter_service_worker.js
+++ docs/flutter_service_worker.js
@@ -24,7 +24,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "65619887eb6810bc047dd19754d8fe8c
 "canvaskit/skwasm.wasm": "39dd80367a4e71582d234948adc521c0",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "83d881c1dbb6d6bcd6b42e274605b69c",
-"flutter_bootstrap.js": "93819a7f096d105136a4a69444d15d30",
+"flutter_bootstrap.js": "73d214d0dc43ce862f58ff2f9985d17c",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
